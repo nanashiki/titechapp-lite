e358be47b8bc2f64318e1f0e2d3eb95083f4ac84
nanashiki <nanashiki.app@gmail.com>
2020-08-16 14:31:52 +0900

    Initial Commit

---
+++ TitechAppLite/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TitechAppLite
+//
+//  Created by nanashiki on 2020/08/16.
+//  Copyright © 2020 nanashiki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
