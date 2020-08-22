4a5bc9029d31c5453d03408dd7bc1f0e7fdd8b39
nanashiki <nanashiki.app@gmail.com>
2020-08-22 18:30:20 +0900

    Add ViewModel and Model

---
+++ TitechAppLite/Utility/HourMinute.swift
@@ -0,0 +1,14 @@
+//
+//  HourMinute.swift
+//  TitechAppLite
+//
+//  Created by nanashiki on 2020/08/22.
+//  Copyright © 2020 nanashiki. All rights reserved.
+//
+
+import Foundation
+
+struct HourMinute {
+    let hour: Int
+    let minute: Int
+}
--- TitechAppLite/View/Components/EventRow.swift
+++ TitechAppLite/View/Components/EventRow.swift
@@ -9,16 +9,18 @@
 import SwiftUI
 
 struct EventRow: View {
+    let event: Event
+
     var body: some View {
         HStack(spacing: 0) {
             Rectangle()
                 .foregroundColor(Color("main"))
                 .frame(width: 5)
             VStack(alignment: .leading, spacing: 24) {
-                Text("00:00")
+                Text("\(String(format: "%02d", event.startAt.hour)):\(String(format: "%02d", event.startAt.minute))")
                     .font(.system(size: 13))
                     .foregroundColor(Color("textMain"))
-                Text("00:00")
+                Text("\(String(format: "%02d", event.endAt.hour)):\(String(format: "%02d", event.endAt.minute))")
                     .font(.system(size: 13))
                     .foregroundColor(Color("textSub"))
             }
@@ -26,11 +28,11 @@ struct EventRow: View {
             .padding(.leading, 16)
             
             VStack(alignment: .leading, spacing: 24) {
-                Text("電気的モデリングとシミュレーション hogehoge")
+                Text(event.title)
                     .lineLimit(1)
                     .font(Font.system(size: 15))
                     .foregroundColor(Color("textMain"))
-                Text("RC回路シミュレーション")
+                Text(event.description)
                     .lineLimit(1)
                     .font(.system(size: 13))
                     .foregroundColor(Color("textSub"))
@@ -40,7 +42,7 @@ struct EventRow: View {
 
             Spacer()
 
-            Text("W833,G114")
+            Text(event.location)
                 .lineLimit(2)
                 .font(.system(size: 15))
                 .foregroundColor(Color("main"))
@@ -52,6 +54,15 @@ struct EventRow: View {
 
 struct EventRow_Previews: PreviewProvider {
     static var previews: some View {
-        EventRow()
+        EventRow(
+            event: Event(
+                id: UUID().uuidString,
+                title: "電気的モデリングとシミュレーション",
+                description: "RC回路シミュレーション",
+                startAt: HourMinute(hour: 9, minute: 0),
+                endAt: HourMinute(hour: 10, minute: 30),
+                location: "W833,G114"
+            )
+        )
     }
 }
